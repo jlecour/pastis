94051ad9a4d2b9f22424545d8cd4b4d67a90b5ca
Jérémy Lecour <jeremy.lecour@gmail.com>
2013-03-10 23:33:17 +0100

    rails new pastis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pastis::Application.config.secret_key_base = '98bd905007294e2f8c477d1d36f0eb5934f87ba6697b284aba65e6f608b25a5d05e2f3a03088b868beb37b92116b9eb5ef2735fc1d550b9251a7caad04b07899'
